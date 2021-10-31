95e104ea60c7fbd0b33967a8596279b93d25aba1
Sannidhya <shubhradasgupta8@gmail.com>
2021-10-31 09:44:27 +0530

    Explored some more button attributes, this is really interesting

--- buttons.py
+++ buttons.py
@@ -6,8 +6,8 @@ def Click():
     label = Label(root, text="Congrats! U clicked!!!", fg="yellow", bg="green")
     label.pack()
 
-myButton = Button(root, text="Click Me!!", padx=50, pady=20, command=Click, fg="White", bg="Black")
+myButton = Button(root, text="Click Me!!", pady=10, command=Click, fg="White", bg="Black",activebackground="cyan", relief=GROOVE)
 
-myButton.pack()
+myButton.pack(side=LEFT)
 
 root.mainloop()
